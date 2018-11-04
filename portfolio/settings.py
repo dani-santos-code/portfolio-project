f8d130cb2e14ca6745df1af2c86fb8cfe4106ea2
Daniele de Araujo dos Santos <danisantos@Danieles-MacBook-Pro-2.local>
2018-11-04 10:47:16 +0100

    reverted changes

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'danisantos',
         'USER': 'postgres',
-        'PASSWORD': 'password',
+        'PASSWORD': 'django1234',
         'HOST': 'localhost',
         'PORT': '5432',
     }
